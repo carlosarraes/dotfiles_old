993b67ac40981dfdc7d077616057510ec25a7112
carraes <carraeshb@gmail.com>
2022-12-19 00:21:20 -0300

    build: fugitive, treesitter

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -1,13 +1,22 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
+  -- Packer Mngr
   use 'wbthomason/packer.nvim'
 
+  -- Telescope
   use {
   	'nvim-telescope/telescope.nvim', tag = '0.1.0',
   	requires = { {'nvim-lua/plenary.nvim'} }
   }
   use "nvim-telescope/telescope-file-browser.nvim"
+  
+  -- Treesitter
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
+  -- Theme
   use 'navarasu/onedark.nvim'
+
+  -- Misc
+  use 'tpope/vim-fugitive'
 end)
